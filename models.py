8a4b8be0d13967142a3a6de72befd0249e38e567
Nikhil Ganta <nikhilganta95@gmail.com>
2021-05-23 21:01:00 -0600

    Database Path Updated

--- models.py
+++ models.py
@@ -2,8 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-database_name = "castingagency"
-database_path = "postgresql://{}@{}/{}".format('postgres', 'localhost:5432', database_name)
+# database_name = "castingagency"
+# database_path = "postgresql://{}@{}/{}".format('postgres', 'localhost:5432', database_name)
+database_path = "postgres://wyiutypddswsme:19d4037b1e9f15491fc94a4c5163fa9277d93f24d36b801ff98d6e52a9fc7f1d@ec2-54-160-96-70.compute-1.amazonaws.com:5432/d79h1jeemeilmq"
 
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
