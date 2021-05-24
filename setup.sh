17788252f52e7bbbbf3b91a41040cbabe4bfca5a
Nikhil Ganta <nikhilganta95@gmail.com>
2021-05-23 21:23:25 -0600

    DB migrate test

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 export AUTH0_DOMAIN="udacityfswd.us.auth0.com"
 export AUTH0_CLIENT_ID="ocgpCzK2u3hJ1iAm7mipsJQJ29Q1FSQ1"
+export DATABASE_URL="postgres://wyiutypddswsme:19d4037b1e9f15491fc94a4c5163fa9277d93f24d36b801ff98d6e52a9fc7f1d@ec2-54-160-96-70.compute-1.amazonaws.com:5432/d79h1jeemeilmq"
